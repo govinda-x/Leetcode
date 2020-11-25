1c356d6a9f2077631251c01d358f41aa35935b7f
Govinda-29 <govindaagarwal29@gmail.com>
2020-11-25 16:17:45 +0530

    Running sum of 1d array

--- running-sum-of-1d-array.java
+++ running-sum-of-1d-array.java
@@ -1,10 +1,10 @@
-class Solution {
+class Solution 
+{
     public int[] runningSum(int[] nums) {
         for(int i =1; i<nums.length; i++)
         {
             nums[i] = nums[i]+nums[i-1];
         }
         return nums;
-        
     }
 }
