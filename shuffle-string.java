cf74eca7ccd5613f6bd9c1224c7122aef5527cbc
Govinda Agarwal <55809816+govinda-x@users.noreply.github.com>
2020-11-25 13:57:06 +0530

    working commit - Created using LeetHub

---
+++ shuffle-string.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String restoreString(String s, int[] indices) 
+    {
+        char [] arr = new char[s.length()];
+        for(int i=0; i<s.length(); i++)
+        {
+            char temp = s.charAt(i);
+            int pos = indices[i];
+            arr[pos]= temp;
+        }
+        return new String(arr); 
+    }
+}
