931ef1feaa7eaa00d8183dd8553f4438b246f823
Govinda Agarwal <55809816+govinda-x@users.noreply.github.com>
2020-11-25 13:54:36 +0530

    working commit - Created using LeetHub

---
+++ n-repeated-element-in-size-2n-array.java
@@ -0,0 +1,27 @@
+class Solution 
+{
+    public int repeatedNTimes(int[] A) 
+    {
+        int ans = 0, max=0;
+        HashMap<Integer, Integer> h = new HashMap<Integer, Integer>();
+        for(int i =0; i<A.length; i++)
+        {
+            if(!h.containsKey(A[i]))
+            {
+                h.put(A[i],1);
+            }
+            h.put(A[i], h.get(A[i])+1);
+        }
+        max = Collections.max(h.values());
+        for(int i =0; i<A.length; i++)
+        {
+            if(max == h.get(A[i]))
+            {
+                ans = A[i];
+            }
+        }
+        return ans;
+        
+        
+    }
+}
