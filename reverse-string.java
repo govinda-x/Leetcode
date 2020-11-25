e9e1bc1cfc58699f7e7640592a566514b29bf854
Govinda Agarwal <55809816+govinda-x@users.noreply.github.com>
2020-11-25 13:52:22 +0530

    working commit - Created using LeetHub

---
+++ reverse-string.java
@@ -0,0 +1,17 @@
+class Solution {
+    public void reverseString(char[] s) 
+    {
+        int len = s.length;
+        int i= 0, j=len-1;
+        while(i<j)
+        {
+            char temp = s[i];
+            s[i]=s[j];
+            s[j]=temp;
+            i++;
+            j--;
+        }
+        System.out.println(s);
+        
+    }
+}
