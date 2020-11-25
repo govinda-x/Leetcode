8fcf6ceb08673c4448a6e1b069ce6a2aa2dbaa45
Govinda Agarwal <55809816+govinda-x@users.noreply.github.com>
2020-11-25 13:34:04 +0530

    working commit - Created using LeetHub

---
+++ powx-n.java
@@ -0,0 +1,20 @@
+class Solution {
+    public double myPow(double x, int n) 
+    {
+       if(n<0 && n>Integer.MIN_VALUE) {
+            x=1/x;
+            n=-(n);
+        }
+        if(n==0){
+            return 1;
+        }
+        if(n==1) {
+            return x;
+        }
+        double ans=myPow(x,n/2);
+        if(n%2==0) {
+            return ans*ans;
+        }
+        return x*ans*ans;
+    }
+}
