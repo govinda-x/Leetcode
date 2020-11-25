75c6812010543cd5822e62a02a5d670100c015cf
Govinda Agarwal <55809816+govinda-x@users.noreply.github.com>
2020-11-25 13:46:07 +0530

    working commit - Created using LeetHub

---
+++ palindrome-number.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isPalindrome(int n) 
+    {
+        int temp = n;
+        if(n<0)
+        {
+            return false;
+        }
+        if(n<10 && n>=0)
+        {
+            return true;
+        }
+        int num = 0;
+        while(n!=0)
+        {
+            int a = n%10;
+            num = num*10+a;
+            n=n/10;
+        }
+        if(temp == num)
+        {
+            return true;
+        }
+        return false;
+        
+        
+    }
+}
