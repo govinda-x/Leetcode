121b56ba7e06dca0ef857011550043b95342ecec
Govinda Agarwal <55809816+govinda-x@users.noreply.github.com>
2020-11-25 13:58:58 +0530

    working commit - Created using LeetHub

---
+++ defanging-an-ip-address.java
@@ -0,0 +1,20 @@
+class Solution 
+{
+    public String defangIPaddr(String address) 
+    {
+        int l = address.length();
+        String n = "";
+        for(int i=0; i<l; i++)
+        {
+            if(address.charAt(i)=='.')
+            {
+                n=n+"[.]";
+            }
+            else
+            {
+                n=n+address.charAt(i);
+            }
+        }
+        return n;
+    }
+}
