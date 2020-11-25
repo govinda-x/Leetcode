84bc090508046822b4703b81fb892389e953fee7
Govinda Agarwal <55809816+govinda-x@users.noreply.github.com>
2020-11-25 13:46:36 +0530

    working commit - Created using LeetHub

---
+++ pascals-triangle-ii.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<Integer> getRow(int rowIndex) 
+    {
+        List<Integer> ans = new ArrayList<Integer>();
+        ans.add(1);
+        if(rowIndex == 0 )
+        {
+            return ans;
+        }
+        List<Integer> prev = getRow(rowIndex-1);
+        for(int i=1; i<prev.size(); i++)
+        {
+            ans.add(prev.get(i-1)+prev.get(i));
+        }
+        ans.add(1);
+        return ans;
+        
+    }
+}
