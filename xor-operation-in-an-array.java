d8354c751cf6429774a447ad143a5dd7f12d1bbc
Govinda Agarwal <55809816+govinda-x@users.noreply.github.com>
2020-11-25 13:55:31 +0530

    working commit - Created using LeetHub

---
+++ xor-operation-in-an-array.java
@@ -0,0 +1,13 @@
+class Solution 
+{
+    public int xorOperation(int n, int start) 
+    {
+        int xor=0;
+        for(int i=0; i<n; i++)
+        {
+            int num = start+2*i;
+            xor=xor^num;
+        }
+        return xor;
+    }
+}
