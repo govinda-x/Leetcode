104f62e55678919b20ee4b6cdefe4467f29e8ec3
Govinda Agarwal <55809816+govinda-x@users.noreply.github.com>
2020-11-25 13:53:31 +0530

    working commit - Created using LeetHub

---
+++ hamming-distance.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        int mod = 1000000007;
+        int sum = 0;
+        for (int bitPos = 0; bitPos <=31; bitPos++) 
+        {
+            int One = 0, Zero = 0;
+            if (x & (1 << bitPos))
+            {
+                One++;
+            }
+            else 
+            {
+                Zero++;
+            }
+            if (y & (1 << bitPos))
+            {
+                One++;
+            }
+            else 
+            {
+                Zero++;
+            }
+            sum = (sum + ((2LL * One * Zero) % mod))%mod;
+        }        
+        return sum/2;
+        
+    }
+};
