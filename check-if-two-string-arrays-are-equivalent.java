14fcc83545e3c64359dc649a1bf5d42c3410d440
Govinda Agarwal <55809816+govinda-x@users.noreply.github.com>
2020-11-25 13:58:29 +0530

    working commit - Created using LeetHub

---
+++ check-if-two-string-arrays-are-equivalent.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean arrayStringsAreEqual(String[] word1, String[] word2) 
+    {
+        String w1="", w2="";
+        for(int i=0; i<word1.length; i++)
+        {
+            w1 = w1+word1[i];
+        }
+        for(int i=0; i<word2.length; i++)
+        {
+            w2 = w2+word2[i];
+        }
+        if(w1.equals(w2))
+        {
+            return true;
+        }
+        return false;
+    }
+}
