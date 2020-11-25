6f31edfb39315bf9945697d9d89f652bf86909fc
Govinda Agarwal <55809816+govinda-x@users.noreply.github.com>
2020-11-25 13:38:19 +0530

    working commit - Created using LeetHub

---
+++ single-number-ii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& A) {
+        int result = 0, count;
+        for(int  i=0; i<32; i++)
+        {
+            count =0;
+            for(int j =0 ; j< A.size(); j++)
+            {
+                if((A[j] >> i) & 1 == 1)
+                {
+                    count++;
+                }
+            }
+            result+=(count%3)<<i;
+        }
+        return result;
+        
+    }
+};
