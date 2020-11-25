9c783636dd23dbfc884bf2928c8daa1e6da3202f
Govinda Agarwal <55809816+govinda-x@users.noreply.github.com>
2020-11-25 13:37:08 +0530

    working commit - Created using LeetHub

---
+++ container-with-most-water.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int maxArea(int[] A) 
+    {
+        if(A.length == 1)
+        {
+            return 0;
+        }
+        int max_area = Integer.MIN_VALUE;
+        int i =0, j= A.length-1;
+        while(i<j)
+        {
+            int area = Math.min(A[i],A[j])*(j-i);
+            max_area = Math.max(max_area,area);
+            
+            if(A[i]>A[j])
+            {
+                j--;
+            }
+            else
+            {
+                i++;
+            }
+        }
+        return max_area;
+    }
+}
