6169ab41e1637ce01b36f2c2635944021de47587
Govinda Agarwal <55809816+govinda-x@users.noreply.github.com>
2020-11-25 13:51:45 +0530

    working commit - Created using LeetHub

---
+++ average-salary-excluding-the-minimum-and-maximum-salary.java
@@ -0,0 +1,20 @@
+class Solution {
+    public double average(int[] salary) 
+    {
+        if(salary.length<=2)
+        {
+            return (double)0.0000;
+        }
+        int n = salary.length;
+        int c=0;
+        double avg = 0;
+        Arrays.sort(salary);
+        for(int i =1; i<n-1; i++)
+        {
+            c++;
+            avg+=salary[i];
+        }
+        double res = avg/c;
+        return res;
+    }
+}
