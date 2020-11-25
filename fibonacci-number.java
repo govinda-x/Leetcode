ec3ebcca6dbbcc0a3e3273336cce6401c2775b21
Govinda Agarwal <55809816+govinda-x@users.noreply.github.com>
2020-11-25 13:51:29 +0530

    working commit - Created using LeetHub

---
+++ fibonacci-number.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int fib(int n) 
+    {
+        if(n ==0)
+        {
+            return 0;
+        }
+        if(n==1 || n==2)
+        {
+            return 1;
+        }
+        int s3 = 0, s1 =1, s2 =1;
+        for(int i=3; i<=n; i++)
+        {
+            s3= s1+s2;
+            s1= s2;
+            s2=s3;
+        }
+        return s3;
+        
+    }
+}
