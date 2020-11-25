753d8046e7b024cc0461a263f61470a5aa55e00b
Govinda-29 <govindaagarwal29@gmail.com>
2020-11-25 16:16:07 +0530

    Remove Element

--- remove-element.java
+++ remove-element.java
@@ -1,5 +1,6 @@
 class Solution {
-    public int removeElement(int[] nums, int val) {
+    public int removeElement(int[] nums, int val) 
+    {
     int i = 0;
     for (int j = 0; j < nums.length; j++) {
         if (nums[j] != val)
@@ -9,5 +10,5 @@ class Solution {
         }
     }
     return i;
-}
+    }
 }
