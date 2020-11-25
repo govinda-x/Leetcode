2825b83f6c88a28486e865a82f1f1d19a9fe0e89
Govinda Agarwal <55809816+govinda-x@users.noreply.github.com>
2020-11-25 13:47:37 +0530

    working commit - Created using LeetHub

---
+++ pascals-triangle.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<List<Integer>> generate(int numRows) 
+    {
+        List<List<Integer>> ans = new ArrayList<List<Integer>>();
+        if(numRows==0)
+        {
+            return ans;
+        }
+        ArrayList<Integer> prev = new ArrayList<Integer>();
+        prev.add(1);
+        ans.add(prev);
+        for(int i=2; i<=numRows; i++)
+        {
+            ArrayList<Integer> curr = new ArrayList<Integer>();
+            curr.add(1);
+            for(int j=0 ; j<prev.size()-1; j++)
+            {
+                curr.add(prev.get(j)+prev.get(j+1));
+            }
+            curr.add(1);
+            ans.add(curr);
+            prev = curr;           
+        }
+        return ans;
+    }
+}
