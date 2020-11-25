5245088cbbaa4014006f0a67e6da8486568d0617
Govinda Agarwal <55809816+govinda-x@users.noreply.github.com>
2020-11-25 13:39:32 +0530

    working commit - Created using LeetHub

---
+++ search-insert-position.java
@@ -0,0 +1,15 @@
+class Solution 
+{
+    public int searchInsert(int[] nums, int target) 
+    {
+        int i=0;
+        for(i=0; i<nums.length; i++)
+        {
+            if(target<=nums[i])
+            {
+                return i;
+            }
+        }
+        return i;
+    }
+}
