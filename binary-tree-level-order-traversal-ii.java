ad7e6064fe37631a6dd3eba178458812a370c8d8
govinda-x <govindaagarwal2999@gmail.com>
2021-06-02 00:44:38 +0530

    Binary Tree Level Order Traversal II

---
+++ binary-tree-level-order-traversal-ii.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> ans = new ArrayList<List<Integer>>();
+        if(root == null) return ans;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        while(true){
+            int count = q.size();
+            if(count==0) break;
+            List<Integer> cols = new ArrayList<>();
+            while(count>0){
+                TreeNode node = q.poll();
+                cols.add(node.val);
+                if(node.left!=null) q.add(node.left);
+                if(node.right!=null) q.add(node.right);
+                count--;
+            }
+            ans.add(cols);
+        }
+        Collections.reverse(ans);
+        return ans;
+    }
+}
